e7f03045e95acfcaa932b92837a98034c64213c4
sezRR <sezer.tetik2005@gmail.com>
2025-03-13 21:34:43 +0300

    Fix failing tests

--- projects/llm-chat-service/src/main/java/dev/sezrr/examples/llmchatservice/LlmChatService.java
+++ projects/llm-chat-service/src/main/java/dev/sezrr/examples/llmchatservice/LlmChatService.java
@@ -1,22 +1,14 @@
 package dev.sezrr.examples.llmchatservice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.modulith.Modulithic;
-import org.springframework.modulith.core.ApplicationModules;
 
 @Modulithic
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class LlmChatService {
-    public static void main(String[] args) {
-//        ApplicationModules modules = ApplicationModules.of(LlmChatService.class);
-//        modules.verify();
-//        modules.forEach(System.out::println);
-//        
+    public static void main(String[] args) {        
         SpringApplication.run(LlmChatService.class, args);
     }
 }
--- projects/llm-chat-service/src/test/java/dev/sezrr/examples/llmchatservice/LlmChatServiceTests.java
+++ projects/llm-chat-service/src/test/java/dev/sezrr/examples/llmchatservice/LlmChatServiceTests.java
@@ -5,8 +5,7 @@ import dev.sezrr.examples.llmchatservice.modules.aimodel.repository.SupportedMod
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +26,13 @@ class SupportedModelControllerTest {
         // Configure your mock as needed and perform requests with mockMvc.
         mockMvc.perform(get("/api/v1/supported-model"))
                 .andExpect(status().isOk());
+    }
+    
+    @Test
+    void testModulithDependencies() {
+        ApplicationModules modules = ApplicationModules.of(LlmChatService.class);
+        modules.verify();
+        modules.forEach(System.out::println);
     }
 }
 
